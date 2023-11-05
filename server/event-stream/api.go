b0700a83f335f65fe5ca1d7ad2b35ff2aa3a7449
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-11-06 00:35:11 +0300

    complete the event engine

--- server/event-stream/api.go
+++ server/event-stream/api.go
@@ -1,1 +1,31 @@
 package event_stream
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/turistikrota/service.booking/app/command"
+)
+
+func (s srv) OnBookingValidationSucceed(data []byte) {
+	fmt.Println("OnBookingValidationSucceed")
+	cmd := command.BookingValidationSucceedCmd{}
+	err := json.Unmarshal(data, &cmd)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	_, _ = s.app.Commands.BookingValidationSucceed(context.Background(), cmd)
+}
+
+func (s srv) OnBookingValidationFail(data []byte) {
+	fmt.Println("OnBookingValidationFail")
+	cmd := command.BookingValidationFailedCmd{}
+	err := json.Unmarshal(data, &cmd)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	_, _ = s.app.Commands.BookingValidationFailed(context.Background(), cmd)
+}
