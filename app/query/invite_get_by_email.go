8e6f46036e02f101a94e2ca40f3e3fd67e20a744
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-11-06 23:44:06 +0300

    add invite get by email query

--- app/query/invite_get_by_email.go
+++ app/query/invite_get_by_email.go
@@ -1,1 +1,29 @@
 package query
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/turistikrota/service.booking/domains/invite"
+)
+
+type InviteGetByEmailQuery struct {
+	UserEmail string
+}
+
+type InviteGetByEmailRes struct {
+	Invites []*invite.Entity
+}
+
+type InviteGetByEmailHandler cqrs.HandlerFunc[InviteGetByEmailQuery, *InviteGetByEmailRes]
+
+func NewInviteGetByEmailHandler(repo invite.Repository) InviteGetByEmailHandler {
+	return func(ctx context.Context, query InviteGetByEmailQuery) (*InviteGetByEmailRes, *i18np.Error) {
+		res, err := repo.GetByEmail(ctx, query.UserEmail)
+		if err != nil {
+			return nil, err
+		}
+		return &InviteGetByEmailRes{Invites: res}, nil
+	}
+}
