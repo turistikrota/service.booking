313124932f3205d16b5b9a0d79bf6da527215367
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-11-07 00:46:16 +0300

    add booking pay timeout cmd

--- app/command/booking_pay_timeout.go
+++ app/command/booking_pay_timeout.go
@@ -1,1 +1,27 @@
 package command
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/turistikrota/service.booking/domains/booking"
+)
+
+type BookingPayTimeoutCmd struct {
+	BookingUUID string `json:"bookingUUID"`
+}
+
+type BookingPayTimeoutRes struct{}
+
+type BookingPayTimeoutHandler cqrs.HandlerFunc[BookingPayTimeoutCmd, *BookingPayTimeoutRes]
+
+func NewBookingPayTimeoutHandler(repo booking.Repo) BookingPayTimeoutHandler {
+	return func(ctx context.Context, cmd BookingPayTimeoutCmd) (*BookingPayTimeoutRes, *i18np.Error) {
+		err := repo.MarkExpired(ctx, cmd.BookingUUID)
+		if err != nil {
+			return nil, err
+		}
+		return &BookingPayTimeoutRes{}, nil
+	}
+}
