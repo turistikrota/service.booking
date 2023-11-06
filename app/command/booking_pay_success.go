390c11b14b808d89612e34855fb892a143014156
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-11-07 00:45:43 +0300

    add booking pay success cmd

--- app/command/booking_pay_success.go
+++ app/command/booking_pay_success.go
@@ -1,1 +1,27 @@
 package command
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/turistikrota/service.booking/domains/booking"
+)
+
+type BookingPaySuccessCmd struct {
+	BookingUUID string `json:"bookingUUID"`
+}
+
+type BookingPaySuccessRes struct{}
+
+type BookingPaySuccessHandler cqrs.HandlerFunc[BookingPaySuccessCmd, *BookingPaySuccessRes]
+
+func NewBookingPaySuccessHandler(repo booking.Repo) BookingPaySuccessHandler {
+	return func(ctx context.Context, cmd BookingPaySuccessCmd) (*BookingPaySuccessRes, *i18np.Error) {
+		err := repo.MarkPaid(ctx, cmd.BookingUUID)
+		if err != nil {
+			return nil, err
+		}
+		return &BookingPaySuccessRes{}, nil
+	}
+}
