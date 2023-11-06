55e0c661591cea4ce88ca8f1a4bbfbb15a9e5b22
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-11-07 00:51:53 +0300

    add cqrs app base

--- app/app.go
+++ app/app.go
@@ -1,6 +1,9 @@
 package app
 
-import "github.com/turistikrota/service.booking/app/command"
+import (
+	"github.com/turistikrota/service.booking/app/command"
+	"github.com/turistikrota/service.booking/app/query"
+)
 
 type Application struct {
 	Commands Commands
@@ -9,9 +12,33 @@ type Application struct {
 
 type Commands struct {
 	BookingCreate            command.BookingCreateHandler
+	BookingCancel            command.BookingCancelHandler
+	BookingMarkPrivate       command.BookingMarkPrivateHandler
+	BookingPayTimeout        command.BookingPayTimeoutHandler
+	BookingPaySuccess        command.BookingPaySuccessHandler
+	BookingMarkPublic        command.BookingMarkPublicHandler
+	BookingRemoveGuest       command.BookingRemoveGuestHandler
+	BookingGuestMarkPublic   command.BookingGuestMarkPublicHandler
+	BookingGuestMarkPrivate  command.BookingGuestMarkPrivateCmd
 	BookingValidationSucceed command.BookingValidationSucceedHandler
 	BookingValidationFailed  command.BookingValidationFailedHandler
+
+	InviteCreate command.InviteCreateHandler
+	InviteUse    command.InviteUseHandler
+	InviteDelete command.InviteDeleteHandler
 }
 
 type Queries struct {
+	InviteGetByBookingUUID   query.InviteGetByBookingUUIDHandler
+	InviteGetByEmail         query.InviteGetByEmailHandler
+	InviteGetByUUID          query.InviteGetByUUIDHandler
+	BookingAdminList         query.BookingAdminListHandler
+	BookingAdminView         query.BookingAdminViewHandler
+	BookingCheckAvailability query.BookingCheckAvailabilityHandler
+	BookingListByOwner       query.BookingListByOwnerHandler
+	BookingListByPost        query.BookingListByPostHandler
+	BookingListByUser        query.BookingListByUserHandler
+	BookingListMyAttendees   query.BookingListMyAttendeesHandler
+	BookingListMyOrganized   query.BookingListMyOrganizedHandler
+	BookingView              query.BookingViewHandler
 }
