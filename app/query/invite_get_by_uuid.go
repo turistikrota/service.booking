3a2e31b4064733099c92f838f7cfb1e3cd1e6a69
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-11-06 23:53:20 +0300

    add get by uuid query

--- app/query/invite_get_by_uuid.go
+++ app/query/invite_get_by_uuid.go
@@ -1,1 +1,29 @@
 package query
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/turistikrota/service.booking/domains/invite"
+)
+
+type InviteGetByUUIDQuery struct {
+	UUID string `params:"uuid" validate:"required,object_id"`
+}
+
+type InviteGetByUUIDRes struct {
+	Invite *invite.Entity
+}
+
+type InviteGetByUUIDHandler cqrs.HandlerFunc[InviteGetByUUIDQuery, *InviteGetByUUIDRes]
+
+func NewInviteGetByUUIDHandler(repo invite.Repository) InviteGetByUUIDHandler {
+	return func(ctx context.Context, query InviteGetByUUIDQuery) (*InviteGetByUUIDRes, *i18np.Error) {
+		res, err := repo.GetByUUID(ctx, query.UUID)
+		if err != nil {
+			return nil, err
+		}
+		return &InviteGetByUUIDRes{Invite: res}, nil
+	}
+}
