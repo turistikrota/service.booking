e0fecfe25f7530b41bfb68c49c8031e2259c2860
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-11-07 00:07:23 +0300

    add booking list by user query

--- domains/booking/dtos.go
+++ domains/booking/dtos.go
@@ -1,1 +1,13 @@
 package booking
+
+type BookingListDto struct{}
+
+type BookingViewDto struct{}
+
+type BookingAdminViewDto struct{}
+
+type BookingAdminListDto struct{}
+
+func (e *Entity) ToListDto() BookingListDto {
+	return BookingListDto{}
+}
