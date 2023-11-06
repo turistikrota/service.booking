553e66fffc1d7461f5ac531a25ff9cb9bcb901c9
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-11-06 20:32:58 +0300

    mark public command added

--- app/command/booking_mark_public.go
+++ app/command/booking_mark_public.go
@@ -1,1 +1,34 @@
 package command
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/turistikrota/service.booking/domains/booking"
+)
+
+type BookingMarkPublicCmd struct {
+	UserUUID string `params:"-"`
+	UUID     string `params:"uuid" validate:"required,object_id"`
+}
+
+type BookingMarkPublicRes struct{}
+
+type BookingMarkPublicHandler cqrs.HandlerFunc[BookingMarkPublicCmd, *BookingMarkPublicRes]
+
+func NewBookingMarkPublicHandler(factory booking.Factory, repo booking.Repo) BookingMarkPublicHandler {
+	return func(ctx context.Context, cmd BookingMarkPublicCmd) (*BookingMarkPublicRes, *i18np.Error) {
+		_, exists, err := repo.GetDetailWithUser(ctx, cmd.UUID, cmd.UserUUID)
+		if err != nil {
+			return nil, err
+		}
+		if exists != nil && !*exists {
+			return nil, factory.Errors.OnlyAdminCanDoThisAction()
+		}
+		if err := repo.MarkPublic(ctx, cmd.UUID); err != nil {
+			return nil, err
+		}
+		return &BookingMarkPublicRes{}, nil
+	}
+}
