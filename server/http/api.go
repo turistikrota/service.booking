9497bfe5ca29b4c3529b625d6fde39eb724a8268
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-11-07 20:55:25 +0300

    add remove guest http handler

--- server/http/api.go
+++ server/http/api.go
@@ -63,3 +63,19 @@ func (h srv) BookingMarkPublic(ctx *fiber.Ctx) error {
 	}
 	return result.SuccessDetail(Messages.Success.Ok, res)
 }
+
+func (h srv) BookingGuestRemove(ctx *fiber.Ctx) error {
+	detail := command.BookingDetailCmd{}
+	h.parseParams(ctx, &detail)
+	cmd := command.BookingRemoveGuestCmd{}
+	h.parseBody(ctx, &cmd)
+	cmd.UserUUID = current_user.Parse(ctx).UUID
+	cmd.UserName = current_account.Parse(ctx).Name
+	cmd.UUID = detail.UUID
+	res, err := h.app.Commands.BookingRemoveGuest(ctx.UserContext(), cmd)
+	if err != nil {
+		l, a := i18n.GetLanguagesInContext(*h.i18n, ctx)
+		return result.Error(h.i18n.TranslateFromError(*err, l, a))
+	}
+	return result.SuccessDetail(Messages.Success.Ok, res)
+}
